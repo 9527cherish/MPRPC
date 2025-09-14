65ba4faf1696e98f10262c5af5b9d6561644ec7b
9527cherish <488655173@qq.com>
2025-09-14 23:35:01 +0800

    init project

---
+++ example/callee/userService.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "user.pb.h"
+
+class UserService : public fixbug::UserServiceRpc
+{
+
+    bool Login(std::string name, std::string pwd)
+    {
+        std::cout << "" << std::endl;
+        std::cout << "name:" << name << "pwd:" << pwd << std::endl;
+    }
+
+    void Login(::google::protobuf::RpcController* controller,
+                const ::fixbug::LoginRequest* request,
+                ::fixbug::LoginResponse* response,
+                ::google::protobuf::Closure* done) override
+    {
+        std::string name = request->name();
+        std::string pwd = request->pwd();
+        fixbug::ResultCode* code = response->mutable_result();
+        code->set_errcode(0);
+        code->set_errmsg("");
+        bool loginResult = Login(name, pwd);
+        response->set_success(loginResult);
+        done->Run();
+    }
+};  
